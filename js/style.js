4cc85511997548fc495b21732e17c8eec404b104
Xilas <aaron_mallett@administrators-MacBook-Pro-25.local>
2014-11-14 11:28:06 -0600

    panel1
    Added most of the content for panel 1, navigation is just a placeholder
    image for now until i figure out how I want to go about making it.

---
+++ js/style.js
@@ -0,0 +1,82 @@
+var DOWN_ARROW_SIZE = 50;
+
+$(document).ready(function(){
+
+	$(".full-page-panel").height($(window).height());
+
+	// call helper functions to vertically align content
+	// in each panel
+	setSpacerDiv1();
+
+	setSpacerDiv2();
+
+	setSpacerDiv3();
+
+	setSpacerDiv4();
+
+});
+
+$(window).resize(function(){
+
+	$(".full-page-panel").height($(window).height());
+
+	// call helper functions to vertically align content
+	// in each panel
+	setSpacerDiv1();
+
+	setSpacerDiv2();
+
+	setSpacerDiv3();
+
+	setSpacerDiv4();
+
+});
+
+/*
+ calculating the height of each panel's spacer
+ div so that the elements are roughly centered
+ values are obtained by adding the height of all elements
+ in that panel
+*/
+function setSpacerDiv1(){
+	// ele heights
+	// 218
+	// 32
+	// 149
+	// 140
+	var contentHeight = $('#rightCol').outerHeight(true);
+
+	var emptySpace = $(window).height() - contentHeight;
+
+	$("#spacer1").height(emptySpace/2);
+}
+
+function setSpacerDiv2(){
+	var contentHeight = $('#leftColumn').outerHeight(true);
+
+	var emptySpace = $(window).height() - contentHeight;
+
+	$("#spacer2").height(emptySpace/2);
+}
+
+function setSpacerDiv3(){
+	var contentHeight = $('#boxText').outerHeight(true) + $('div.form').outerHeight(true);
+
+	var emptySpace = $(window).height() - contentHeight;
+
+	$("#spacer3").height(emptySpace/2);
+}
+
+function setSpacerDiv4(){
+	var contentHeight = $('#rightColumn').outerHeight(true);
+
+	var emptySpace = $(window).height() - contentHeight;
+
+	$("#spacer4").height(emptySpace/2);
+}
+
+
+
+
+
+
